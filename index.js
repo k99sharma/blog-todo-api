a3ed5c061b5b97cd3d874087f2134901bf025d95
Kalash Sharma <kalash.strt@gmail.com>
2023-07-24 17:51:56 +0530

    feat: simple express server
     simple express server is created in index file
    
     - added readme file
     - added gitignore file
     - yarn is initialized

---
+++ index.js
@@ -0,0 +1,10 @@
+// imports
+import express from "express";
+
+// app
+const app = express();
+
+// server is listening
+app.listen(3000, ()=>{
+    console.log("Server is listening at PORT: 3000");
+});
